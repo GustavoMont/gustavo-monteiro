75d08453743daf68697c475c9bf692e4525d64fc
GustavoMont <lgmluisgm@gmail.com>
2025-01-11 19:15:09 -0300

    :tada: init project

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <q>
+        Aqui será lar de vários poemas dessa mente, que não vale tanta coisa.
+      </q>
+    </div>
+  );
+}
