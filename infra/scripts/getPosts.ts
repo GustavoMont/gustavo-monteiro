912c8f429322a0cbc5a14658dc83e4e276b49836
GustavoMont <lgmluisgm@gmail.com>
2025-01-14 20:03:56 -0300

    feat: create `api/v1/posts/:slug` endpoint

--- infra/scripts/getPosts.ts
+++ infra/scripts/getPosts.ts
@@ -3,22 +3,30 @@ import { readFile, readdir } from "node:fs/promises";
 import { resolve } from "node:path";
 import matter from "gray-matter";
 
+async function getPostByFilename(filename: string): Promise<Post> {
+  const path = resolve("_posts");
+  const slug = filename.replace(".md", "");
+  const postFile = await readFile(resolve(path, filename), "utf-8");
+
+  const { content, data } = matter(postFile);
+  return {
+    slug,
+    content,
+    title: data.title,
+    publishedDate: data["published-date"],
+    updatedDate: data["updated-date"],
+    tags: data.tags,
+    type: data.type,
+  };
+}
+
+export async function getPostBySlug(slug: string): Promise<Post> {
+  return getPostByFilename(slug + ".md");
+}
+
 export async function getPosts() {
   const path = resolve("_posts");
   const files = await readdir(path, "utf-8");
-  const postPromises = files.map<Promise<Post>>(async (filename) => {
-    const slug = filename.replace(".md", "");
-    const postFile = await readFile(resolve(path, filename), "utf-8");
-    const { content, data } = matter(postFile);
-    return {
-      slug,
-      content,
-      title: data.title,
-      publishedDate: data["published-date"],
-      updatedDate: data["updated-date"],
-      tags: data.tags,
-      type: data.type,
-    };
-  });
+  const postPromises = files.map<Promise<Post>>(getPostByFilename);
   return Promise.all(postPromises);
 }
