a93e3a198d1254fc17984363735624f475b19289
GustavoMont <lgmluisgm@gmail.com>
2025-01-19 10:16:17 -0300

    feat: config tailwind style

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <q>
-        Aqui será lar de vários poemas dessa mente, que não vale tanta coisa.
-      </q>
-    </div>
+    <main className="p-8">
+      <div className="prose">
+        <blockquote>
+          <q>
+            Aqui será lar de vários poemas dessa mente, que não vale tanta
+            coisa.
+          </q>
+        </blockquote>
+      </div>
+    </main>
   );
 }
