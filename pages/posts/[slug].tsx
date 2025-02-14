1b559e4675f964313d9f515879b192ea3777d145
GustavoMont <lgmluisgm@gmail.com>
2025-02-13 22:00:17 -0300

    feat: create `TEXT` post style

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,9 +1,34 @@
+import { Post } from "@/@types/post.type";
+import { GetServerSideProps } from "next";
 import React from "react";
+import ReactMarkdown from "react-markdown";
 
-export default function PostPage() {
+type Props = {
+  post: Post;
+};
+
+export default function PostPage({ post }: Props) {
   return (
-    <div className="h-screen w-full flex items-center justify-center text-xl">
-      <p>~(˘▾˘~) Nada aqui ainda ~(˘▾˘~)</p>
-    </div>
+    <main
+      className="prose prose-headings:text-primary prose-code:text-accent prose-code:bg-accent-content
+    prose-blockquote:bg-primary-content prose-blockquote:border-primary prose-blockquote:rounded-md"
+    >
+      <ReactMarkdown>{post.content}</ReactMarkdown>
+    </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const slug = query.slug;
+  if (typeof slug !== "string") {
+    return {
+      notFound: true,
+    };
+  }
+  const response = await fetch(`${process.env.API_URL}/posts/${slug}`);
+  const post: Post = await response.json();
+
+  return {
+    props: { post },
+  };
+};
