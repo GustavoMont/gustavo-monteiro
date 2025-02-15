876c272f6ff659fa6fad26ab35092f39ff6d9421
GustavoMont <lgmluisgm@gmail.com>
2025-02-15 10:07:07 -0300

    feat: make `HR` style more flexable

--- components/post/poetry/HR.tsx
+++ components/post/poetry/HR.tsx
@@ -1,7 +1,16 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
 
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLHRElement>, HTMLHRElement>;
 
 export const HR = (props: Props) => {
-  return <hr {...props} className="my-5 w-2/5 opacity-50 border-secondary" />;
+  return (
+    <hr
+      {...props}
+      className={twMerge(
+        "my-5 w-2/5 opacity-50 border-secondary",
+        props.className,
+      )}
+    />
+  );
 };
