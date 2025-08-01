c7a137f0b5da2d0ca866425d7076146a2428a103
GustavoMont <lgmluisgm@gmail.com>
2025-08-01 10:32:42 -0300

    build: config `tailwind` and `daisyui`

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br" className={montserrat.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
