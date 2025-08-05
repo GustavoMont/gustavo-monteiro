4843cff76a84c5ff5916f801642730bab3f95936
GustavoMont <lgmluisgm@gmail.com>
2025-08-04 21:49:02 -0300

    feat: create post cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { Post, PostTypeEnum } from "@/@types/post";
+import { PostCard } from "@/components/posts/PostCard";
+
+const dummyPosts: Post[] = Array.from({ length: 6 }).map((_, i) => ({
+  slug: `post-${i}`,
+  description: `Lorem ipsum dolor sit amet consectetur. Aliquam faucibus tempor cursus id. ${i}`,
+  title: `Post ${i}`,
+  type: i % 2 === 0 ? PostTypeEnum.POETRY : PostTypeEnum.TEXT,
+  writtenAt: new Date().toISOString(),
+  banner: "https://picsum.photos/600",
+}));
+
 export default function HomePage() {
   return (
-    <>
-      <h1>Opa</h1>
-    </>
+    <ul className="flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:gap-6">
+      {dummyPosts.map((post) => (
+        <li key={post.slug}>
+          <PostCard post={post} />
+        </li>
+      ))}
+    </ul>
   );
 }
