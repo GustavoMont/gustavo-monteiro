24d3bc6b362a4f6baadd45f3221ad4dad26910ab
GustavoMont <lgmluisgm@gmail.com>
2025-08-07 21:32:42 -0300

    feat: add link in avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,7 +42,9 @@ function DesktopHeader() {
 function Presentation() {
   return (
     <div className="flex gap-2 items-center lg:flex-col">
-      <Avatar />
+      <Link href={"/"}>
+        <Avatar />
+      </Link>
       <div className="flex flex-col gap-1">
         <p>Olá, sou o Gustavo, e espero que goste do que eu escrevo</p>
         <Link href={"https://instagram.com/gustavomont_136"} target="_blank">
